e38fa619199c8f77bcc8049cbf4ba62249a899a9
Yifan <yifan.bao@influxdata.nz>
2022-11-14 16:25:16 +1300

    initial stuff

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,24 +1,11 @@
 import Phaser from 'phaser';
 
-export default class Demo extends Phaser.Scene {
+export default class Menu extends Phaser.Scene {
   constructor() {
-    super('GameScene');
-  }
-
-  preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    super('Menu');
   }
 
   create() {
-    const logo = this.add.image(400, 70, 'logo');
-
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1
-    });
+    const logo = this.add.text(350, 70, 'logo');
   }
 }
